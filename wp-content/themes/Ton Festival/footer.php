221f84e6e30640a6dbe80b864f4598841e7d17a1
jemappellepaul <pololeparc@gmail.com>
2020-12-15 10:46:12 +0100

    debut du style

--- wp-content/themes/Ton Festival/footer.php
+++ wp-content/themes/Ton Festival/footer.php
@@ -1,21 +1,44 @@
     </div>
-    <div>
-    <?php the_field('titre'); ?>
-    <?php the_field('contenu'); ?>
-    <?php the_field('deuxieme_colonne'); ?>
-    <?php the_field('troisieme_colonne'); ?>
-    <?php the_field('quatrieme_colonne'); ?>
-    </div>
-    
     <!-- menu pied de page -->
-    <nav class="navbar fixed-bottom navbar-light bg-light">
-    <?php wp_nav_menu([
-                'theme_location' => 'footer',
-                'container' => false,
-                'menu_class' => 'navbar-nav mr-auto'
-            ]); ?>
+    <div class="block_top_footer">
+        <?php wp_nav_menu([
+            'theme_location' => 'footer',
+            'container' => false,
+            'menu_class' => 'block_top_footer'
+        ]); ?>
+        <?php the_field('contenu'); ?>
+
+        <h1 style="color : white;"><?php if (have_rows('premiere_colonne')) :
+                while (have_rows('premiere_colonne')) : the_row();
+                    $sub_value = get_sub_field('contenu');
+                // Do something...
+                endwhile;
+            else :
+            // no rows found
+            echo ('nothing');
+            endif; ?></h1>
+            <h2><?php
+            if( have_rows('premiere_colonne') ):
+                while( have_rows('premiere_colonne') ): the_row();
             
-    <?php wp_footer(); ?>
-    
-</body>
-</html>
+                    // Layout 1.
+                    if( get_row_layout() == 'layout_1' ):
+            
+                        // Layout 1 value.
+                        $value = get_sub_field('sub_field_1');
+            
+                    // Layout 2.
+                    elseif( get_row_layout() == 'layout_2' ):
+            
+                        // Layout 2 value.
+                        $value = get_sub_field('sub_field_2');
+            
+                    endif;
+            
+                endwhile;
+            endif;
+            ?></h2>
+    </div>
+        </body>
+
+        </html>
--- wp-content/themes/Ton Festival/header.php
+++ wp-content/themes/Ton Festival/header.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="<?php echo (get_template_directory_uri() . "/style.css"); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- obtenir le titre de la page -->
-    <?php wp_head(); ?>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="js/script.js"></script>
+    <!-- titre de l'onglet -->
+    <title>TON FESTIVAL</title>
 </head>
-
+<!-- Corps HTML -->
 <body>
-<!-- style de la barre de menu -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#" ><img src="*" alt="logo"/></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- barre menu -->
-            <?php wp_nav_menu([
-                'theme_location' => 'header',
-                'container' => false,
-                'menu_class' => 'navbar-nav mr-auto'
-            ]); ?>
-
-        </div>
-        <!-- formulaire de recherche -->
-        <?= get_search_form(); ?>
-    </nav>
-
-    <div class="container">
+    <div class="index">
+        <!-- header -->
+        <header>
+            <!-- Menu-->
+            <nav class="index">
+                <ul>
+                    <li>
+                        <a href="index.html" title="Home"><?php wp_nav_menu([
+                                                                'theme_location' => 'header',
+                                                                'container' => false,
+                                                                'menu_class' => 'navbar-nav mr-auto'
+                                                            ]); ?></a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <div class="container">
